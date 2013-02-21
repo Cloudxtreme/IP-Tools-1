a58eb7eee0fb70713b7ebdbc04904e8b27d93ff6
Ben Bullock <benkasminbullock@gmail.com>
2013-02-21 15:53:39 +0900

    Initial commit of IP-Tools

---
+++ lib/IP/Tools.pm
@@ -0,0 +1,26 @@
+=encoding UTF-8
+
+=head1 NAME
+
+IP::Tools - abstract here.
+
+=head1 SYNOPSIS
+
+    use IP::Tools;
+
+=head1 DESCRIPTION
+
+=head1 FUNCTIONS
+
+=cut
+package IP::Tools;
+require Exporter;
+@ISA = qw(Exporter);
+@EXPORT_OK = qw//;
+%EXPORT_TAGS = (
+    all => \@EXPORT_OK,
+);
+use warnings;
+use strict;
+our $VERSION = 0.01;
+1;
---
+++ t/IP-Tools.t
@@ -0,0 +1,10 @@
+# This is a test for module IP::Tools.
+
+use warnings;
+use strict;
+use Test::More;
+use IP::Tools;
+done_testing ();
+# Local variables:
+# mode: perl
+# End:
